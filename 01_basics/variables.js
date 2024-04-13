cde8fa42b9766c3bf2d6837cf42e1aab9b2884f1
Debanjan52 <166905753+Debanjan52@users.noreply.github.com>
2024-04-13 09:54:02 +0000

    variables done

---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 14569
+let accountEmail = "debanjan@mail.com"
+var accountPassword = "1519"
+accountCity = "Bengaluru"
+let accountState
+
+// accountId = 2
+
+accountEmail= "bal@mail.com"
+accountPassword= "212121"
+accountCity="kolkata"
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
