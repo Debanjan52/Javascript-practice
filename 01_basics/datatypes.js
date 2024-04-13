87b79faec0daac51d13d0789e6072f86157f74a9
Debanjan52 <166905753+Debanjan52@users.noreply.github.com>
2024-04-13 10:12:54 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+// alert("hello")// we are using node js not browser
+
+// code readablity must be high
+
+let name="debanjan"
+let age= 18
+let isLoggedIn= false
+
+// these areprimitive data types
+// number= 2 to the power 53
+// bigint for very big numbers
+// string use ""
+// boolean = true/false
+// null = standalone value representation of empty value
+// undefined = it is not defined
+// symbol = unique
+
+//object
+
+console.log(typeof null); //type of null is object but type of undefined is undefined//
+
